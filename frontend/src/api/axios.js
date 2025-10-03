4a94b14b111f7023a1800a8117388bf270b4b385
Chatchai <jjunejune7799@gmail.com>
2025-10-04 00:05:09 +1000

    Test 2

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://http://54.252.244.102",
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5001/api",
   withCredentials: false,
 });
 
